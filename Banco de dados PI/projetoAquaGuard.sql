99b3ef453a4de77ba0f43100bbad6a50d7f11ed0
HeloSalgado <heloisa.salgado@sptech.school>
2023-09-12 10:37:37 -0300

    Update banco de dados

--- Banco de dados PI/projetoAquaGuard.sql
+++ Banco de dados PI/projetoAquaGuard.sql
@@ -1,34 +1,34 @@
-create database AquaGuard;
-use AquaGuard;
+CREATE DATABASE AquaGuard;
+USE AquaGuard;
 
-create table usuarioEmpresa (
-    id_usuario int primary key auto_increment,
-    email_usuario varchar(100) not null,
-    senha_usuario varchar(50) not null);
+CREATE TABLE usuarioEmpresa (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    email_usuario VARCHAR(100) NOT NULL,
+    senha_usuario VARCHAR(50) NOT NULL);
 
-create table dadosEmpresa (
-	id_empresa int primary key auto_increment,
-    cnpj_empresa  float unique,
-	nome_empresa varchar(40),
-    email_empresa varchar(100));
+CREATE TABLE dadosEmpresa (
+	id_empresa INT PRIMARY KEY AUTO_INCREMENT,
+    cnpj_empresa  FLOAT UNIQUE,
+	nome_empresa VARCHAR(40),
+    email_empresa VARCHAR(100));
     
-create table enderecoEmpresa(
-	id_endereco int primary key auto_increment,
-    cep_empresa int,
-    rua_empresa varchar(50),
-    bairro_empresa varchar (40),
-    municipio_empresa varchar(40),
-    complemento_empresa varchar(20));
+CREATE TABLE enderecoEmpresa(
+	id_endereco INT PRIMARY KEY AUTO_INCREMENT,
+    cep_empresa INT,
+    rua_empresa VARCHAR(50),
+    bairro_empresa VARCHAR (40),
+    municipio_empresa VARCHAR(40),
+    complemento_empresa VARCHAR(20));
 
     
-create table leituraSensores (
-	id_sensores int primary key auto_increment,
-	sensor1 int,
-    sensor2 int,
-    dt_atual datetime default current_timestamp);
+CREATE TABLE leituraSensores (
+	id_sensores INT PRIMARY KEY AUTO_INCREMENT,
+	sensor1 INT,
+    sensor2 INT,
+    dt_atual DATETIME DEFAULT CURRENT_TIMESTAMP);
    
 -- INSERT 
-insert into usuarioEmpresa values
+INSERT INTO usuarioEmpresa VALUES
 (null, 'maria.guardiao@sptech.school', '12345'),
 (null, 'heloisa.salgado@sptech.school', '54321'),
 (null, 'luan.araujo@sptech.school', '67890'),
@@ -37,7 +37,7 @@ insert into usuarioEmpresa values
 (null, 'giovanni.santos@sptech.school', '76543'),
 (null, 'gabriel.barreto@sptech.school', '89012');
 
-insert into dadosEmpresa values
+INSERT INTO dadosEmpresa VALUES
 (null, 93365395574574, 'Americanas', 'americanas@gmail.com'),
 (null, 20156387000187, 'Assaí Atacadista', 'assaiatacadista@gmail.com'),
 (null, 06808342000119, 'Carrefour', 'carrefou@gmail.com'),
@@ -46,7 +46,7 @@ insert into dadosEmpresa values
 (null, 04344358700014, 'Supermercado Dia', 'supermecadodia@gmail.com'),
 (null, 04443587000165, 'Extra', 'extra@gmail.com');
 
-insert into enderecoEmpresa values
+INSERT INTO enderecoEmpresa VALUES
 (null, 76873064, 'Avenida Urupá', 'Boa Vista', 'Caruaru', 'A' ),
 (null, 60192010, 'Rua Vilebaldo Aguiar', 'Santa Lúcia', 'Rio Branco', 'B'),
 (null, 45822230, 'Rua São Lourenço', 'Pedreira', 'Araraquara', 'C'),
@@ -55,26 +55,26 @@ insert into enderecoEmpresa values
 (null, 49067246, 'Porto Dantas', 'Fazendinha', 'Eldorado', 'F'),
 (null, 81200080, 'Rua João Kososki', 'Operário', 'Glória', 'G');
 
-insert into leituraSensores (sensor1, sensor2) values 
+INSERT INTO leituraSensores (sensor1, sensor2) VALUES 
 (0,0),
 (1,0),
 (1,1);
 
 -- SELECT
-select * from usuarioEmpresa;
-select * from dadosEmpresa;
-select * from enderecoEmpresa;
-select * from leituraSensores;
+SELECT * FROM usuarioEmpresa;
+SELECT * FROM dadosEmpresa;
+SELECT * FROM enderecoEmpresa;
+SELECT * FROM leituraSensores;
 
-select id_usuario as ID, email_usuario  as Email, senha_usuario as Senha from usuarioEmpresa;
-select cnpj_empresa as CNPJ, nome_empresa as Nome, email_empresa as Email from dadosEmpresa; 
-select cep_empresa as CEP, rua_empresa as Rua, bairro_empresa as Bairro, municipio_empresa as Município, complemento_empresa as Complemento from enderecoEmpresa; 
-select sensor1 as 'Sensor de presença 1', sensor2 as 'Sensor de presença 2', dt_atual as 'Data e Hora' from leituraSensores;
+SELECT id_usuario AS ID, email_usuario AS Email, senha_usuario AS Senha FROM usuarioEmpresa;
+SELECT cnpj_empresa AS CNPJ, nome_empresa AS Nome, email_empresa AS Email FROM dadosEmpresa; 
+SELECT cep_empresa AS CEP, rua_empresa AS Rua, bairro_empresa AS Bairro, municipio_empresa AS Município, complemento_empresa AS Complemento FROM enderecoEmpresa; 
+SELECT sensor1 AS 'Sensor de presença 1', sensor2 AS 'Sensor de presença 2', dt_atual AS 'Data e Hora' FROM leituraSensores;
 
-select * from usuarioEmpresa where email_usuario like 'g%';
-select * from dadosEmpresa where nome_empresa like '%a';
-select * from enderecoEmpresa where rua_empresa like '%a_';
-select * from leituraSensores where sensor1 like '%1%';
+SELECT * FROM usuarioEmpresa WHERE email_usuario LIKE 'g%';
+SELECT * FROM dadosEmpresa WHERE nome_empresa LIKE '%a';
+SELECT * FROM enderecoEmpresa WHERE rua_empresa LIKE '%a_';
+SELECT * FROM leituraSensores WHERE sensor1 = 1;
 
-    
+DROP DATABASE AquaGuard;
     
