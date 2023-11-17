a7093a466458525243f030fca951945590d1ac02
HeloSalgado <heloisa.salgado@sptech.school>
2023-11-17 16:01:01 -0300

    Alteração da modelagem feita pelo grupo

--- Sensor Presença/arduino_code/arduido_dadosBrutos/arduido_dadosBrutos.ino
+++ Sensor Presença/arduino_code/arduido_dadosBrutos/arduido_dadosBrutos.ino
@@ -4,24 +4,17 @@ int leitura1 = 0;
 int porta2 = 7;
 int leitura2 = 0;
 
-int soma = 0;
-
 void setup() {
   Serial.begin(9600);
-
 }
 
 void loop() {
 leitura1 = map(digitalRead(porta1), 1, 0, 0, 1);
-
 leitura2 = map(digitalRead(porta2), 1, 0, 0, 1);
 
-
 Serial.print(leitura1);
-Serial.print('; ');
+Serial.print(';');
 Serial.println(leitura2);
-
-}
 
 delay(500);
 }
